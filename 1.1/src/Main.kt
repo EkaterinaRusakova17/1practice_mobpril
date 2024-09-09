7e9bdd51360ea0555b3852411361f4c5f9a2e64e
EkaterinaRusakova17 <145272537+EkaterinaRusakova17@users.noreply.github.com>
2024-09-09 23:00:38 +0700

    Add files via upload

---
+++ 1.1/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    print("Введите целое число:")
+    val number = readln()
+    val firstNumber = number.first().digitToInt()
+    val secondNumber = number.last().digitToInt()
+    val result = firstNumber + secondNumber
+    println(result)
+}
