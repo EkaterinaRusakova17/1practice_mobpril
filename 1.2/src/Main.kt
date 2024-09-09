4fde3246b9e94d9898857a44fcb207ea6b277b90
EkaterinaRusakova17 <145272537+EkaterinaRusakova17@users.noreply.github.com>
2024-09-09 23:28:50 +0700

    Add files via upload

---
+++ 1.2/src/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+    var sum = 0
+    var count = 0
+    var input: String?
+
+    do {
+        print("Введите число: ")
+        input = readLine()
+
+        if (input != null) {
+            val number = input.toInt()
+            if (number != 0) {
+                sum += number
+                count++
+            }
+        }
+    } while (input?.toInt() != 0)
+
+    println("Количество введенных чисел: $count")
+    println("Их сумма: $sum")
+    if (count != 0) {
+        val average = sum.toDouble() / count
+        println("Среднее арифметическое: $average")
+    } else {
+        println("Среднее арифметическое не может быть вычислено, так как не было введено ни одного числа.")
+    }
+}
